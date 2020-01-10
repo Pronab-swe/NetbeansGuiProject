b1d2bf4b6e9817821a9416edeeacebee37e41a08
Pronab21 <bpronab21@fmail.com>
2020-01-10 23:13:39 +0600

    printInitial

--- src/firstguiproject/FirstGuiProject.java
+++ src/firstguiproject/FirstGuiProject.java
@@ -8,6 +8,8 @@ public class FirstGuiProject {
     public static void main(String[] args) {
        Addition ad = new Addition();
        ad.show();
+       
+       System.out.print("fgdfgdfgfsdgfsd");
     }
     
 }
